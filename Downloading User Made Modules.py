8922a2302caf900f7a738cd9d73eec98eaea111d
BlueMarbleCode <116312747+BlueMarbleCode@users.noreply.github.com>
2024-09-30 18:16:08 -0400

    Add files via upload

---
+++ Downloading User Made Modules.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# Intermediate level topics:
+#   downloading new modules, made by other users
+# =============================================================================
+# When python is downloaded to your computer, it comes with built in
+# functions, such as the print() or enuerate() functions
+#
+# These built in functions are often insufficient for our purposes, and many
+# online users have created amazing modules that are freely accessible,
+# for instance, the pygame module allows us to create fun games n python.
+#
+# To find useful modules, we can head to:
+#   https://pypi.org
+# A free site that allows users to upload modules they've created
+# =============================================================================
+# In the Visual Studio Code Terminal:
+#   pip install (name of module)
+#
+#   (or if the above didn't work)
+#
+#   python -m pip install (name of module)
+#
+# For instance, we would first download the pygame module, then open a comand window
+# and type: pip install pygame
+# ==================================================================================
