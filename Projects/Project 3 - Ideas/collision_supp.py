d988fded03131f28b9243d2714d7b057b6b84976
BlueMarbleCode <116312747+BlueMarbleCode@users.noreply.github.com>
2024-09-30 18:22:29 -0400

    Delete Projects/Project 3 - Ideas directory

--- Projects/Project 3 - Ideas/collision_supp.py
+++
@@ -1,8 +0,0 @@
-def collision(b = (0,0,0,0),a = (0,0,0,0)):
-      ''' a,b are rectangles,(x,y,l,h)
-       this code checks if a enters b's domain '''
-      
-      if b[0] <= a[0] <= b[0] + b[2] or b[0] <= a[0] + a[2] <= b[0] + b[2]:
-            if b[1] <= a[1] + a[3] <= b[1] + b[3] or b[1] <= a[1] <= b[1] + b[3]:
-                  return True      
-      return False
